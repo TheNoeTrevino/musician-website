2e54a0c8eaa8e7b9a9bc3f6a28b42524e7c45b5b
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-18 16:07:13 -0600

    add: user entity

---
+++ backend/src/main/java/com/project/backend/models/Users.java
@@ -0,0 +1,33 @@
+package com.project.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+public class Users {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @NotNull(message = "First name on a user cannot be null")
+  @Column(name = "first_name")
+  private String firstName;
+
+  @NotNull(message = "Last name on a user cannot be null")
+  @Column(name = "last_name")
+  private String lastName;
+
+  @NotNull(message = "Password on a user cannot be null")
+  @Column(name = "password")
+  private String password;
+
+  @NotNull(message = "Email on a user cannot be null")
+  @Column(name = "email")
+  private String email;
+}
