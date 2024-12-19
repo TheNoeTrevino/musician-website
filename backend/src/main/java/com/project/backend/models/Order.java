bfd6ca3632b2a97ca7f9d39df16939a7a77c2509
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-19 12:11:11 -0600

    add(db): piece <-> orders relationship
    many to many

---
+++ backend/src/main/java/com/project/backend/models/Order.java
@@ -0,0 +1,36 @@
+package com.project.backend.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "orders") // NOTE: order gets canned as a reserve keyword
+public class Order {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @NotNull(message = "An order must have a total price")
+  @Column(name = "price")
+  public String composer;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private Users user;
+
+  @ManyToMany(mappedBy = "orders")
+  private Set<Piece> pieces = new HashSet<>();
+}
