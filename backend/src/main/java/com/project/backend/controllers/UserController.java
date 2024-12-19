9ec146e6a94f834674c4ed9cc17446689f464da4
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-18 20:36:36 -0600

    add(controller): find/delete by id

--- backend/src/main/java/com/project/backend/controllers/UserController.java
+++ backend/src/main/java/com/project/backend/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.project.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.backend.DTOs.UserDTO;
+import com.project.backend.services.UserService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,9 +18,18 @@ import org.slf4j.LoggerFactory;
 public class UserController {
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+  @Autowired
+  UserService userService;
+
   @GetMapping("/{id}")
   public UserDTO getUserById(@PathVariable Long id) {
-    logger.info("Searching for member with ID: {}", id);
-    return UserService.getUserById();
+    logger.info("Searching for user with ID: {}", id);
+    return userService.getUserById(id);
+  }
+
+  @DeleteMapping("/{id}")
+  public UserDTO deleteUserById(@PathVariable Long id) {
+    logger.info("Deleteing user with ID: {}", id);
+    return userService.deleteUserById(id);
   }
 }
