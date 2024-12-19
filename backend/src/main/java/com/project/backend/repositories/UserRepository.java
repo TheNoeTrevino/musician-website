76915704b6a7c7268b3cb7e6d4803cf035913557
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-18 20:35:27 -0600

    add: user repositroy methods
    - delete by id
    - find by role

--- backend/src/main/java/com/project/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/project/backend/repositories/UserRepository.java
@@ -1,5 +1,28 @@
 package com.project.backend.repositories;
 
-public class UserRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.project.backend.DTOs.UserDTO;
+import com.project.backend.eums.Role;
+import com.project.backend.models.Users;
+
+public interface UserRepository extends JpaRepository<Users, Long>,
+    JpaSpecificationExecutor<Users> {
+
+  @Modifying
+  @Query("delete User u where u.id = :id")
+  int deleteUserById(@Param(value = "id") Long id);
+
+  @Query("""
+      SELECT firstName, lastName, emailAddress from users
+      where role = 'USER'
+      """)
+  List<UserDTO> findByRole(Role role);
 
 }
