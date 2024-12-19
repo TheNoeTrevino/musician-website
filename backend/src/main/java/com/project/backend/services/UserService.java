681ceef6a5315241627d4997a3213646344ef41b
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-18 20:36:02 -0600

    add: user service methods
    - delete by id
    - get by id

--- backend/src/main/java/com/project/backend/services/UserService.java
+++ backend/src/main/java/com/project/backend/services/UserService.java
@@ -1,5 +1,37 @@
 package com.project.backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.backend.DTOs.UserDTO;
+import com.project.backend.exceptions.NotFoundException;
+import com.project.backend.mappers.UserMapper;
+import com.project.backend.repositories.UserRepository;
+
 public class UserService {
+
+  @Autowired
+  UserRepository UserRepo;
+
+  @Autowired
+  UserMapper UserMapper;
+
+  public UserDTO getUserById(Long userId) {
+    UserRepo.findById(userId);
+
+    UserDTO userDTO = UserMapper
+        .UserToDTO(UserRepo.findById(userId).orElseThrow(() -> new NotFoundException("user", userId)));
+
+    return userDTO;
+  };
+
+  public UserDTO deleteUserById(Long userId) {
+
+    UserDTO userDTO = UserMapper
+        .UserToDTO(UserRepo.findById(userId).orElseThrow(() -> new NotFoundException("user", userId)));
+
+    UserRepo.deleteUserById(userId);
+
+    return userDTO;
+  };
 
 }
