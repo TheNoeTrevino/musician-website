d7c9612e575cc337ef7cbb591d0e6ca002ba9084
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-19 12:10:14 -0600

    refactor: pieces -> piece

--- backend/src/main/java/com/project/backend/models/Piece.java
+++ backend/src/main/java/com/project/backend/models/Piece.java
@@ -1,16 +1,22 @@
 package com.project.backend.models;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 
+// TODO: add instrumentation
 @Entity
-public class Pieces {
+public class Piece {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
@@ -30,4 +36,12 @@ public class Pieces {
   @NotNull(message = "A piece must have a date in which it was composed")
   @Column(name = "date_composed")
   public LocalDate dateComposed;
+
+  @ManyToMany
+  @JoinTable(
+    name = "order_pieces",
+    joinColumns = @JoinColumn(name = "order_id"),
+    inverseJoinColumns = @JoinColumn(name = "piece_id")
+  )
+  private Set<Order> orders = new HashSet<>();
 }
