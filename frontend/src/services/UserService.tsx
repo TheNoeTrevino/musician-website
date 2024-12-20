4b052a925eb25fa6cdb77d0ebb2ecff4b7909ecf
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-20 12:51:08 -0600

    add(fe): delete by user

--- frontend/src/services/UserService.tsx
+++ frontend/src/services/UserService.tsx
@@ -22,6 +22,37 @@ export const UserService = {
     } catch (error) {
       console.log(error);
       throw new Error("Something went wrong fetching the user information");
+
+  async deleteUserById(userId: number): Promise<UserDTO> {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/users/user/${userId}`,
+        {
+          method: "DELETE",
+        },
+      );
+
+      console.log(`deleting user: ${userId}`);
+
+      if (!response.ok) {
+        alert(
+          `response not ok, Something went wrong deleting the user with id: ${userId}`,
+        );
+      }
+
+      const userData: UserDTO = await response.json();
+
+      console.log(response);
+
+      return userData;
+    } catch (error) {
+      console.log(error);
+      throw new Error(
+        `Something went wrong deleting the user with id: ${userId}`,
+      );
+    }
+  },
+
     }
   },
 };
