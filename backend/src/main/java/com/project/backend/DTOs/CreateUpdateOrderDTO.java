7c29f8599e6427418d8c487364b0e88cadd1f228
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-19 19:55:02 -0600

    add(dto): create order dto

---
+++ backend/src/main/java/com/project/backend/DTOs/CreateUpdateOrderDTO.java
@@ -0,0 +1,18 @@
+package com.project.backend.DTOs;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateUpdateOrderDTO {
+
+  public float price;
+
+  @NotNull(message = "an order must have the user id with it.")
+  private Long userId;
+
+  @NotNull(message = "The order must have the pieces Ids with it")
+  private List<Long> pieceIds;
+}
