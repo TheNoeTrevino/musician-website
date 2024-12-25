03639ab2d10c104fc188a6a2e13b0879f22976b5
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-24 18:03:22 -0600

    add: create user endpoint

--- backend/src/main/java/com/project/backend/controllers/UserController.java
+++ backend/src/main/java/com/project/backend/controllers/UserController.java
@@ -5,13 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.backend.DTOs.UserDTO;
+import com.project.backend.DTOs.CreateUpdateUserDTO;
 import com.project.backend.eums.Role;
 import com.project.backend.services.UserService;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -42,5 +47,12 @@ public class UserController {
   public ResponseEntity<List<UserDTO>> getUsersByRole(@RequestParam(required = true) Role role) {
     logger.info("Fetching users with role: {}", role);
     return ResponseEntity.ok(userService.getUsersByRole(role));
+  }
+
+  // TODO: this should be a create DTO rather than the actual entity
+  @PostMapping("/")
+  public ResponseEntity<UserDTO> createUser(@Valid @RequestBody CreateUpdateUserDTO user) {
+    logger.info("Creating user");
+    return ResponseEntity.ok(userService.createUser(user));
   }
 }
