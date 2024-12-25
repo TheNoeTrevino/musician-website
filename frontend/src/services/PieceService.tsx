2951082b6f0b5aa88a198da1995843d9e2d12ce2
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-25 17:15:44 -0600

    add(pieces): basic crud operation

---
+++ frontend/src/services/PieceService.tsx
@@ -0,0 +1,54 @@
+import { PieceDTO } from "../dtos/dtos";
+
+// TODO: make some test buttons for this
+export const PieceService = {
+  async getPiecesWithSort(
+    orderBy: string,
+    sortOrder: string,
+  ): Promise<PieceDTO[]> {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/pieces/?orderBy=${orderBy}&sortOrder=${sortOrder}`,
+        {
+          method: "GET",
+        },
+      );
+      console.log(
+        `fetching pices with params orderBy: ${orderBy}, sortOrder: ${sortOrder}`,
+      );
+
+      if (!response.ok) {
+        alert("Something went wrong fetching pieces");
+      }
+
+      const piecesData: PieceDTO[] = await response.json();
+
+      return piecesData;
+    } catch (error) {
+      throw new Error(`Something went wrong fetching the pieces: ${error}`);
+    }
+  },
+
+  async getPieceById(pieceId: number): Promise<PieceDTO> {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/pieces/piece/${pieceId}`,
+        {
+          method: "GET",
+        },
+      );
+
+      if (!response.ok) {
+        alert("Something went wrong fetching pieces");
+      }
+
+      const pieceData: PieceDTO = await response.json();
+
+      return pieceData;
+    } catch (error) {
+      throw new Error(
+        `something went wrong fetching piece with id: ${pieceId}. Error: ${error}`,
+      );
+    }
+  },
+};
