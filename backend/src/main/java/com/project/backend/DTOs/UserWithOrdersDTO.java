78d2ebf4896fd54812f4c7652b8d9495f4e0a7ef
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-31 17:53:56 -0600

    add(dto): user w orders

---
+++ backend/src/main/java/com/project/backend/DTOs/UserWithOrdersDTO.java
@@ -0,0 +1,19 @@
+package com.project.backend.DTOs;
+
+import java.util.List;
+
+import com.project.backend.eums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UserWithOrdersDTO {
+  private long userId;
+  private String firstName;
+  private String lastName;
+  private String emailAddress;
+  private Role role;
+  private List<OrderDTO> orders;
+}
