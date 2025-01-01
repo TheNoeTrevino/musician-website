6bc36a8f824be85a05c7994af85e0b14329cc603
TheNoeTrevino <noetrevino.music@gmail.com>
2025-01-01 04:20:34 -0600

    FEAT: dynamic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,37 @@ import Cart from "./pages/cart/Cart";
 import Showcase from "./pages/showcase/Showcase";
 import Checkout from "./pages/checkout/Checkout";
 import ErrorPage from "./pages/ErrorPage";
+import { PieceService } from "./services/PieceService";
+import { useEffect, useState } from "react";
+import { PieceDTO } from "./dtos/dtos";
 
 function App() {
+  const fetchPieces = async () => {
+    const pieces = await PieceService.getAllPieces();
+    setPieces(pieces);
+  };
+
+  const [pieces, setPieces] = useState<PieceDTO[]>([]);
+
+  useEffect(() => {
+    fetchPieces();
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/showcase" element={<Showcase />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<ErrorPage />} />
+        {pieces.map((piece) => (
+          <Route
+            key={piece.pieceId}
+            path={`${piece.title.replace(" ", "-")}`}
+            element={<Showcase piece={piece} />}
+          ></Route>
+        ))}
       </Routes>
     </Router>
   );
--- frontend/src/pages/showcase/Showcase.tsx
+++ frontend/src/pages/showcase/Showcase.tsx
@@ -1,8 +1,19 @@
-const Showcase = () => {
+import { PieceDTO } from "../../dtos/dtos";
+
+interface ShowcaseProps {
+  piece: PieceDTO;
+}
+
+// TODO: dynamically get the image
+// and figure out a better way to store time in the db
+const Showcase = ({ piece }: ShowcaseProps) => {
   return (
     <div>
-      <div>Showcase</div>
-      <div>Showcase</div>
+      <div>{piece.composer}</div>
+      <div>{piece.title}</div>
+      <div>{piece.yearComposed}</div>
+      <div>{piece.timeLength}</div>
+      <div>{piece.difficultyGrade}</div>
     </div>
   );
 };
