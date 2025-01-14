ccce22ac49f0255a26a61067804994e8f4301744
Noe Trevino <noetrevino.music@gmail.com>
2025-01-14 11:30:22 -0600

    fix: lorem->actual about

--- frontend/src/components/AboutSection.tsx
+++ frontend/src/components/AboutSection.tsx
@@ -4,24 +4,44 @@ const AboutSection = () => {
       <div className="flex flex-col w-full  gap-10  z-10 pt-14 ">
         <p className="text-7xl text-white">About</p>
         <span className="text-textGray text-xl">
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Mollis interdum
-          nulla pharetra nulla mi commodo curae urna. Vehicula libero vitae
-          fames ullamcorper class lacus sed mi dictumst. Facilisis egestas
-          luctus tortor erat ultricies etiam viverra tellus purus? Magnis
-          aliquet curabitur dignissim est himenaeos mi. Velit ridiculus ornare
-          magna libero varius facilisi. Sit placerat scelerisque habitasse velit
-          purus pulvinar mus imperdiet hac. Vehicula etiam leo iaculis mus
-          egestas consectetur. Lacinia tincidunt laoreet lacinia pharetra;
-          platea velit. Ad litora faucibus neque proin sollicitudin facilisi.
-          Pharetra dictum sollicitudin tristique nam sociosqu dui eu quisque
-          donec. Platea pulvinar viverra massa, commodo bibendum facilisi odio.
-          Amet placerat lectus torquent, arcu morbi maximus est tristique.
-          Malesuada arcu gravida enim magnis mattis libero curabitur ad
-          malesuada. Suscipit id neque conubia risus finibus magnis. Enim
-          tincidunt interdum hendrerit at imperdiet risus per ultrices? Finibus
-          vel nibh dictum, sit sem felis sociosqu nulla. Odio ridiculus lacus,
-          metus nam cubilia lobortis orci? Id nisl commodo eget aptent facilisis
-          quis. Hendrerit elementum in convallis conubia per blandit.
+          <p className="mb-4">
+            Sebastian Havner is an American percussionist, educator, arranger,
+            and composer based out of Denton, TX.
+          </p>
+          <p>
+            Sebastian’s creativity and desire to push the boundaries of
+            percussion music are shown throughout his compositions. He aspires
+            to take percussion music to a new level, integrating new, unique
+            sounds through the use of acoustic sounds and electronic tracks.
+            With all his works, Sebastian’s primary goal is to incite emotion
+            within every audience member, providing the listener with a
+            meaningful experience that transcends traditional compositions.
+            Sebastian aims to extend his writing style to performers of all
+            ages, maintaining the same quality and emotions regardless of age
+            and experience. His compositions have been performed in groups at a
+            high school and collegiate level.
+          </p>
+          <p className="mb-4">
+            Sebastian's inspirations stem from his extensive background as a
+            solo and chamber performer. He is an active performer in the UNT
+            Percussion Studio, performing in notable works including White Pines
+            by Michael Burritt as a soloist, and multiple world premieres.
+          </p>
+          <p className="mb-4">
+            Sebastian has received extensive knowledge and inspiration from his
+            years in Drum Corps. Sebastian is currently a member of the Santa
+            Clara Vanguard for the 2024 season, continuing on his time with the
+            group from the 2022 season. During the 2023 season, he was part of a
+            monumental season with the Casper Troopers, receiving the highest
+            percussion score in corps history. Sebastian’s drum corps experience
+            has influenced his path as a composer and teacher, serving as the
+            Front Ensemble technician for the UNT Green Brigade in 2023.
+            Sebastian
+          </p>
+          <p className="mb-4">
+            Havner is currently pursuing a Bachelor’s degree in Music Education
+            from the University of North Texas.
+          </p>
         </span>
       </div>
 
