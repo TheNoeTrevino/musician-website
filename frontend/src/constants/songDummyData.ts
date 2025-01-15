e32bdaa0e7f97bf71b8827b9994ea40f0a4a8317
Nna² <144557253+NEjim2001@users.noreply.github.com>
2025-01-14 21:17:57 -0600

    fix: confusing name issue

--- frontend/src/constants/songDummyData.ts
+++ frontend/src/constants/songDummyData.ts
@@ -3,7 +3,7 @@ export const songDummyData = [
     title: "Standing on The Shoulders of Giants",
     description:
       "Standing on The Shoulders of Giants is a piece for two marimba soloists with a backing percussion ensemble.The piece is written in a way that while the soloists are in the spotlight, the whole ensemble is very involved andrather than just support the soloists, they have prominent voices that create the piece’s energy.To me, the beauty of music is the emotions it can bring with it. There is a certain almost larger than life feeling Ihave experienced at many points listening to or performing music, when you are completely pulled into a piece itcan be a profound experience. I wrote Standing on the Shoulders of Giants with the intent to invoke thoseemotions in the audience and players while listening or performing.",
-    image: "/albums/1.png",
+    image: "/albums/standing-on-the-shoulders-of-giants.png",
     price: 49.99,
     quantity: 1,
     year: 2023,
@@ -47,7 +47,7 @@ export const songDummyData = [
     title: "Celestial",
     description:
       "Celestial is a percussion sextet written for my alma mater Berkner Highschool. My intention with this piece wasto write a beginner-intermediate level percussion ensemble with the energy and feeling of a professional levelpiece so students can have an enjoyable experience and eventually seamlessly transition to the evolving modernplaystyle.The piece does not follow a strict storyline but one can loosely interpret it as an adventure into space and thereturn back home. It encapsulates the imagery of the cosmos as well as the unknown factor of what dangerscould be out there.",
-    image: "/albums/2.png",
+    image: "/albums/celestial.png",
     price: 29.99,
     quantity: 2,
     year: 2024,
@@ -75,7 +75,7 @@ export const songDummyData = [
     title: "Memento Mori",
     description:
       "Memento Mori is a Latin phrase translating to “Remember that you must die”. I wrote this piece to convey the many different feelings that this idea can cause in a person. At face value, the idea of death can be a very scary and dark thought, although without death, the meaning of life is diminished. Memento Mori aims to show the constant convolution of how death is viewed, while generally intense and “negative” at times the piece diverges to showcase the beauty of death as the finale to someone's story, rather than an undesirable end. I purposefully switch between different tones within the music to convey my own uncertainty with the idea of death. Memento Mori is written for solo marimba with a percussion quartet accompaniment. It is an advanced piece with individual challenges as well as difficult ensemble moments. The soloist weaves in and out of the spotlight, keeping an interesting blend of soloistic moments and full ensemble ideas. I intended to create a compelling soundscape between the warmer timbre of the marimba with the bright sounds of the metallic instruments backing up the soloist, driven forward by the energy of the hand drumming.",
-    image: "/albums/3.png",
+    image: "/albums/memento-mori.png",
     price: 49.99,
     quantity: 1,
     year: 2024,
