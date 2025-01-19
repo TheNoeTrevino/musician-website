2979dc28141c8de08084eb0c8650f2fb3b173e34
Noe Trevino <noetrevino.music@gmail.com>
2025-01-19 14:46:10 -0600

    change: hover consistentcy across icons etc...
    - i increased the scale if the icons/texts were smaller.
    - feels a little more smooth
    - added the effect the the links on the left as well

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,15 +14,24 @@ const Header = () => {
   return (
     <div className="absolute w-screen flex flex-row gap-10 justify-between items-center px-44 text-xl text-center text-white max-md:px-5 h-24 z-20  ">
       <div className="flex gap-10 justify-center items-center self-stretch my-auto font-light whitespace-nowrap min-w-[240px] w-[441px] max-md:max-w-full">
-        <Link to={"#about"} className="cursor-pointer">
+        <Link
+          to={"#about"}
+          className="cursor-pointer hover:scale-110 transition-transform"
+        >
           About
         </Link>
-        <Link to={"/shop"} className="cursor-pointer">
+        <Link
+          to={"/shop"}
+          className="cursor-pointer hover:scale-110 transition-transform"
+        >
           Shop
         </Link>
 
         {/* TODO: make this take us to a contact page instead of the section   */}
-        <Link to={"/contact-me"} className="cursor-pointer">
+        <Link
+          to={"/contact-me"}
+          className="cursor-pointer hover:scale-110 transition-transform"
+        >
           Contact Me
         </Link>
       </div>
@@ -41,25 +50,25 @@ const Header = () => {
         >
           <IconBrandInstagram
             strokeWidth={1}
-            className="cursor-pointer hover:scale-105 transition-transform"
+            className="cursor-pointer hover:scale-125 transition-transform"
           />
         </a>
         <a href="https://www.youtube.com/@sebastianhavner" target="_blank">
           <IconBrandYoutube
             strokeWidth={1}
-            className="cursor-pointer hover:scale-105 transition-transform"
+            className="cursor-pointer hover:scale-125 transition-transform"
           />
         </a>
         <a href="https://soundcloud.com/sebastian-havner" target="_blank">
           <IconBrandSoundcloud
             strokeWidth={1}
-            className="cursor-pointer hover:scale-105 transition-transform"
+            className="cursor-pointer hover:scale-125 transition-transform"
           />
         </a>
         <a href="https://www.facebook.com/sebastian.havner.5" target="_blank">
           <IconBrandFacebook
             strokeWidth={1}
-            className="cursor-pointer hover:scale-105 transition-transform"
+            className="cursor-pointer hover:scale-125 transition-transform"
           />
         </a>
         <Link
@@ -68,7 +77,7 @@ const Header = () => {
         >
           <IconShoppingCart
             strokeWidth={1}
-            className="hover:scale-105 transition-transform "
+            className="hover:scale-125 transition-transform "
           />
           {cartItems.length > 0 && (
             <div className=" bg-secondary  w-5 h-5 bg-primary rounded-full text-xs flex justify-center items-center">
