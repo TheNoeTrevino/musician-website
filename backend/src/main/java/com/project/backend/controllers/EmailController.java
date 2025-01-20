7e52445e528f9aea37a94b466118b33da788ed32
Noe Trevino <noetrevino.music@gmail.com>
2025-01-19 21:25:01 -0600

    start: controller

---
+++ backend/src/main/java/com/project/backend/controllers/EmailController.java
@@ -0,0 +1,10 @@
+package com.project.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// @RequestMapping("/email")
+// public class EmailController {
+//
+// }
