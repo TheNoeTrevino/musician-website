5d89b7806b564431d9ede7e8614f591698f9f785
Noe Trevino <noetrevino.music@gmail.com>
2025-01-20 00:09:58 -0600

    add: email controller and email service

--- backend/src/main/java/com/project/backend/services/EmailService.java
+++ backend/src/main/java/com/project/backend/services/EmailService.java
@@ -1,7 +1,12 @@
 package com.project.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import com.project.backend.DTOs.EmailDTO;
+import com.project.backend.DTOs.EmailResponseDTO;
 
 @Service
 public class EmailService {
@@ -9,8 +14,17 @@ public class EmailService {
   @Autowired
   private JavaMailSender mailSender;
 
-  public void sendEmail(String toEmail, String subject, String body) {
-    SimpleMailSender message = new SimpleMailMessage();
-    message.setFrom("");
+  public EmailResponseDTO sendEmail(EmailDTO email) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setTo("noetrevino.music@gmail.com");
+    message.setFrom(email.getFrom());
+    message.setSubject(email.getSubject());
+    message.setText(email.getMessage());
+    mailSender.send(message);
+
+    EmailResponseDTO response = new EmailResponseDTO();
+    response.setSuccessful(true);
+    response.setReply("Thanks for sending me an email! I will get back to you as soon as I can.");
+    return response;
   }
 }
