a7ab95a1944f13fe8dcdd56ce24b74d28a2e3dff
Noe Trevino <noetrevino.music@gmail.com>
2025-01-23 21:28:15 -0600

    SEBAS: change border

--- frontend/src/pages/shop/Shop.tsx
+++ frontend/src/pages/shop/Shop.tsx
@@ -1,5 +1,4 @@
 import ContactSection from "../../components/ContactSection";
-import SecondaryHeroSection from "../../components/SecondaryHeroSection";
 import SeperationSection from "../../components/SeperationSection";
 import StoreSecton from "../../components/PieceShopSecton";
 import useScrollToTop from "../../hooks/ScrollTop";
@@ -9,8 +8,7 @@ const Shop = () => {
   useScrollToTop();
   return (
     <body className="flex flex-col bg-black">
-      <SecondaryHeroSection backdrop={false} image={null} />
-      <div className="h-px bg-gray-500 w-3/4 self-center my-32"></div>
+      <div className="h-px bg-black w-3/4 self-center my-10"></div>
       <StoreSecton />
       <SeperationSection />
       <ContactSection />
