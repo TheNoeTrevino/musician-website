8395de0ae0496a0866619d9e8074409f3771a312
Noe Trevino <noetrevino.music@gmail.com>
2025-01-23 21:28:15 -0600

    SEBAS: change border

--- frontend/src/pages/shop/Shop.tsx
+++ frontend/src/pages/shop/Shop.tsx
@@ -1,5 +1,4 @@
 import ContactSection from "../../components/ContactSection";
-import SecondaryHeroSection from "../../components/SecondaryHeroSection";
 import SeperationSection from "../../components/SeperationSection";
 import StoreSecton from "../../components/PieceShopSecton";
 
@@ -7,8 +6,7 @@ import StoreSecton from "../../components/PieceShopSecton";
 const Shop = () => {
   return (
     <body className="flex flex-col bg-black">
-      <SecondaryHeroSection backdrop={false} image={null} />
-      <div className="h-px bg-gray-500 w-3/4 self-center my-32"></div>
+      <div className="h-px bg-black w-3/4 self-center my-10"></div>
       <StoreSecton />
       <SeperationSection />
       <ContactSection />
