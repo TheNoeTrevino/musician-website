84b4ae16f84162f68905ae75d9861ee888e3d43a
Noe Trevino <noetrevino.music@gmail.com>
2025-01-30 11:33:00 -0600

    WIP: service goes through the list of products

--- backend/src/main/java/com/project/backend/services/PaymentService.java
+++ backend/src/main/java/com/project/backend/services/PaymentService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.project.backend.DTOs.PaymentRequestDTO;
 import com.project.backend.DTOs.PaymentResponseDTO;
-import com.project.backend.DTOs.ProductDTO;
-import com.project.backend.exceptions.NotFoundException;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Price;
@@ -26,65 +24,52 @@ public class PaymentService {
   public PaymentResponseDTO checkoutProducts(PaymentRequestDTO paymentRequest) {
     Stripe.apiKey = stripeSecret;
 
-    paymentRequest.getProducts().stream().forEach(dto -> {
-      Product product;
-      try {
-        product = Product.retrieve(dto.getId());
-      } catch {
-      }
-    }
-    );
-
-    try {
-      product = Product.retrieve(paymentRequest.getProductId());
-    } catch (StripeException ex) {
-      logger.error("Product retrieval failed: ", ex.getMessage());
-      return PaymentResponseDTO.builder()
-          .status("FAILED")
-          .message("Product not found")
-          .build();
-    }
-
-    Price price = null;
-    try {
-      price = Price.retrieve(product.getDefaultPrice());
-    } catch (StripeException ex) {
-      logger.error("Price retrieval failed: ", ex.getMessage());
-      return PaymentResponseDTO.builder()
-          .status("FAILED")
-          .message("Price not found")
-          .build();
-    }
-
-    SessionCreateParams.LineItem.PriceData.ProductData productData = SessionCreateParams.LineItem.PriceData.ProductData
-        .builder()
-        .setName(product.getName())
-        .build();
-
-    SessionCreateParams.LineItem.PriceData priceData = SessionCreateParams.LineItem.PriceData.builder()
-        .setCurrency(price.getCurrency()) // FIX: right now will always be USD
-        .setUnitAmount(price.getUnitAmount())
-        .setProductData(productData)
-        .build();
-
-    SessionCreateParams.LineItem lineItem = SessionCreateParams.LineItem.builder()
-        .setQuantity(paymentRequest.getQuantity())
-        .setPriceData(priceData)
-        .build();
-
     SessionCreateParams params = SessionCreateParams.builder()
         .setMode(SessionCreateParams.Mode.PAYMENT)
         .setSuccessUrl("http://localhost:5173/shop") // TODO: make a success page
         .setCancelUrl("http://localhost:5173/shop") // TODO: make a cancel page
-        .addLineItem(lineItem)
         .build();
+
+    paymentRequest.getProducts().stream().forEach(dto -> {
+      Product product;
+      try {
+        logger.info("Looking for product with id: " + dto.getId());
+        product = Product.retrieve(dto.getId());
+      } catch (StripeException ex) {
+        throw new RuntimeException("Failed to get product");
+      }
+
+      Price price;
+      try {
+        price = Price.retrieve(product.getDefaultPrice());
+      } catch (StripeException ex) {
+        throw new RuntimeException("Failed to get price");
+      }
+
+      SessionCreateParams.LineItem.PriceData.ProductData productData = SessionCreateParams.LineItem.PriceData.ProductData
+          .builder()
+          .setName(product.getName())
+          .build();
+
+      SessionCreateParams.LineItem.PriceData priceData = SessionCreateParams.LineItem.PriceData.builder()
+          .setCurrency(price.getCurrency()) // FIX: right now will always be USD
+          .setUnitAmount(price.getUnitAmount())
+          .setProductData(productData)
+          .build();
+
+      SessionCreateParams.LineItem lineItem = SessionCreateParams.LineItem.builder()
+          .setQuantity(dto.getQuantity())
+          .setPriceData(priceData)
+          .build();
+
+      params.getLineItems().add(lineItem);
+    });
 
     Session session = null;
 
     try {
       session = Session.create(params);
     } catch (StripeException exception) {
-      // TODO: return a failed stripe response
       logger.error("Payment session creation failed: ", exception.getMessage());
       return PaymentResponseDTO.builder()
           .status("FAILED")
@@ -96,30 +81,7 @@ public class PaymentService {
         .status("SUCCESS")
         .message("Payment session created")
         .sessionId(session.getId())
-        .checkoutUrl(session.getUrl()) // checkout URL
+        .checkoutUrl(session.getUrl()) // checkout URL TODO: take them here in the frontend
         .build();
-  }
-
-  public String getProductById() {
-    Stripe.apiKey = stripeSecret;
-
-    Product product = null;
-    try {
-      product = Product.retrieve("prod_RgMaQVCpahAW2z");
-    } catch (StripeException ex) {
-      logger.error("Payment failed: ", ex.getMessage());
-      throw new NotFoundException(ex.getMessage(), (long) 2);
-    }
-
-    String price;
-
-    try {
-      price = Price.retrieve(product.getDefaultPrice()).toString();
-    } catch (StripeException ex) {
-      logger.error("Payment failed: ", ex.getMessage());
-      throw new NotFoundException(ex.getMessage(), (long) 2);
-    }
-
-    return product.getName() + "\n" + price;
   }
 }
