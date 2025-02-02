42059f9690e026e492d51ca9738b1fe7b72b74a5
Noe Trevino <noetrevino.music@gmail.com>
2025-02-02 13:55:26 -0600

    fix: wrap app in cart provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ToastContainer } from "react-toastify";
+import { CartProvider } from "./services/CartProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
     <ToastContainer />
   </StrictMode>,
 );
