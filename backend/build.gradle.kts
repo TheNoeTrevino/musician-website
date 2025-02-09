c7e69ad2a78bfbbf24952d99ec9233580257a23d
Noe Trevino <noetrevino.music@gmail.com>
2025-02-08 19:14:12 -0600

    add(build): dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,10 @@ repositories {
 }
 
 dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-security:3.4.2")
+  implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+  runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+  runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
   implementation("com.stripe:stripe-java:28.3.0")
   implementation("org.springframework.boot:spring-boot-starter-mail")
   implementation("jakarta.validation:jakarta.validation-api:3.1.0")
