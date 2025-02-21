730d1ca993f3ec494149618e88607cddfec2078c
Noe Trevino <noetrevino.music@gmail.com>
2025-02-21 12:46:50 -0600

    fix: basic auth not working
    - to be honest, i dont know why this works, but i was just trying
    things, and this ended up fixing the issue

--- backend/src/main/java/com/project/backend/services/CustomerDetailsService.java
+++ backend/src/main/java/com/project/backend/services/CustomerDetailsService.java
@@ -1,12 +1,15 @@
 package com.project.backend.services;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.project.backend.models.UserPrincipal;
+import com.project.backend.exceptions.UserNotFoundException;
 import com.project.backend.models.Users;
 import com.project.backend.repositories.UserRepository;
 
@@ -17,9 +20,7 @@ public class CustomerDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    Users user = userRepo.findByUsername(username)
-        .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-
-    return new UserPrincipal(user);
+    Users user = userRepo.findByUsername(username).orElseThrow(() -> new UserNotFoundException("user", username));
+    return new User(user.getUsername(), user.getPassword(), Collections.singleton(user.getAuthority()));
   }
 }
