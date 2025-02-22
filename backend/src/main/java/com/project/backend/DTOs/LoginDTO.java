d0eabcb0f71634a99d47275806156b0fef1839b5
Noe Trevino <noetrevino.music@gmail.com>
2025-02-22 11:19:45 -0600

    add: login contrustor

--- backend/src/main/java/com/project/backend/DTOs/LoginDTO.java
+++ backend/src/main/java/com/project/backend/DTOs/LoginDTO.java
@@ -1,8 +1,10 @@
 package com.project.backend.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginDTO {
   private String username;
   private String password;
