f8415ca779542f755cace04560ba498cf9c4e0ef
Noe Trevino <noetrevino.music@gmail.com>
2025-02-22 06:38:44 -0600

    security: add login to open endpoints

--- backend/src/main/java/com/project/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/project/backend/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.project.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,7 +28,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(customizer -> customizer.disable())
         .authorizeHttpRequests(request -> request
-            .requestMatchers("/users/**", "/login", "/auth/**").permitAll() // permit
+            .requestMatchers("/users/**", "/auth/login", "/pieces/**").permitAll() // permit
             .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -39,5 +41,10 @@ public class SecurityConfig {
     provider.setPasswordEncoder(new BCryptPasswordEncoder(10)); // this is 2^x
     provider.setUserDetailsService(detailsService);
     return provider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
   }
 }
