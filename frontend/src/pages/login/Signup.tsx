b8e7b28539bf5cd2faebe1fb180d5ffec9ff86f5
Noe Trevino <noetrevino.music@gmail.com>
2025-02-22 11:31:16 -0600

    add(routes): signup

---
+++ frontend/src/pages/login/Signup.tsx
@@ -0,0 +1,11 @@
+import SignupSection from "../../components/SignupSection";
+
+const Signup = () => {
+  return (
+    <body className="flex flex-col ">
+      <SignupSection />;
+    </body>
+  );
+};
+
+export default Signup;
