d24c8d40db2beaaa47e8f09d9dd9f15702774f66
Noe Trevino <noetrevino.music@gmail.com>
2025-02-22 06:39:46 -0600

    add(login): controller method

--- backend/src/main/java/com/project/backend/controllers/AuthController.java
+++ backend/src/main/java/com/project/backend/controllers/AuthController.java
@@ -1,19 +1,23 @@
 package com.project.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.backend.DTOs.LoginDTO;
+import com.project.backend.services.UserService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+  @Autowired
+  UserService userService;
+
   @PostMapping("/login")
   public String login(@RequestBody LoginDTO loginDTO) {
-    return "success";
+    return userService.verify(loginDTO);
   }
-
 }
