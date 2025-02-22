592011846bc8664d95fb4cf326cbab5eb44b5df5
Noe Trevino <noetrevino.music@gmail.com>
2025-02-22 11:20:49 -0600

    rm: unused imports

--- backend/src/main/java/com/project/backend/controllers/UserController.java
+++ backend/src/main/java/com/project/backend/controllers/UserController.java
@@ -5,17 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.backend.DTOs.UserDTO;
-import com.project.backend.DTOs.CreateUpdateUserDTO;
 import com.project.backend.services.UserService;
-
-import jakarta.validation.Valid;
 
 import java.util.List;
 
