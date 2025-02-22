d48d7388aedd0090b9c4bb8d6f52adff29dc0210
Noe Trevino <noetrevino.music@gmail.com>
2025-02-22 11:31:16 -0600

    add(routes): signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import About from "./pages/about/About";
 import Success from "./pages/payment/Success";
 import Cancel from "./pages/payment/Cancel";
 import Login from "./pages/login/Login";
+import Signup from "./pages/login/Signup";
 
 function App() {
   const fetchPieces = async () => {
@@ -38,6 +39,7 @@ function App() {
           <Route path="/contact-me" element={<ContactSection />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/success" element={<Success />} />
           <Route path="/cancel" element={<Cancel />} />
           <Route path="*" element={<ErrorPage />} />
