ff121b6f38096dc29f43e5ab3b681f2ca12abc84
TheNoeTrevino <noetrevino.music@gmail.com>
2025-04-18 20:03:25 -0500

    permit all endpoints without authentication

--- backend/src/main/java/com/project/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/project/backend/security/SecurityConfig.java
@@ -33,8 +33,10 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(customizer -> customizer.disable())
         .authorizeHttpRequests(request -> request
-            .requestMatchers("/auth/login", "/auth/register", "/pieces/**").permitAll() // permit
-            .anyRequest().authenticated())
+            // .requestMatchers("/auth/login", "/auth/register", "/pieces/**").permitAll()
+            // // permit
+            // .anyRequest().authenticated())
+            .anyRequest().permitAll())
         .httpBasic(Customizer.withDefaults())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
