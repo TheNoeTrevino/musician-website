8aa2f6cb6b61e875d2da3ef1942bc721812495a0
Noe Trevino <noetrevino.music@gmail.com>
2025-07-10 01:22:36 -0500

    fix: redirect

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,8 +3,8 @@ import Socials from "./Socials";
 const Footer = () => {
   return (
     <div className="flex flex-row bg-reallyBlack px-52">
-      <div className="flex  flex-col grow shrink py-20 text-white ">
-        <div className="  text-5xl max-md:text-4xl">
+      <div className="flex flex-col grow shrink py-20 text-white ">
+        <div className="text-5xl max-md:text-4xl">
           <img src="/logo.png" alt="logo" className="w-64" />
         </div>
         <div className="mt-2 text-xs">© Copyright 2025</div>
--- frontend/src/components/SeperationSection.tsx
+++ frontend/src/components/SeperationSection.tsx
@@ -1,5 +1,5 @@
 const SeperationSection = () => {
-  return <div className=" bg-primary flex flex-row h-60 "></div>;
+  return <div className="bg-primary flex flex-row h-60 "></div>;
 };
 
 export default SeperationSection;
